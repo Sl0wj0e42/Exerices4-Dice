cfdc93efe645b589d914bb39d63148a499567b90
Sl0wj0e42 <91666405+Sl0wj0e42@users.noreply.github.com>
2024-12-09 18:20:53 +0000

    broken but all the dice files are there and does make random number

--- backend/dice-server.js
+++ backend/dice-server.js
@@ -1,9 +1,19 @@
 let port = 8080;
 let express = require('express');
 let app =express();
+const path = require('path');
 
-app.get('/', (req, res) => {
-    res.send('Hello there!!');
+app.set("views", path.join (__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, 'files')));
+
+app.get('/dicepage', (req, res) => {
+    number = Math.floor(Math.random() * 6) + 1;
+
+    res.render("dicepage", { numberResponse: number });
+
+    //res.send('Random number = ' + number.toString());
 });
 
 app.listen(port, () => {
